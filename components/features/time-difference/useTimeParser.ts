3f8c701394c824a8beadd4f167ac3b2db57ba3a6
Stefan <stefan.hauser@tutanota.com>
2021-08-06 13:24:41 +0200

    Feat: Use current time, if end time is not provided

--- components/features/time-difference/useTimeParser.ts
+++ components/features/time-difference/useTimeParser.ts
@@ -3,7 +3,7 @@ import { TimeDifferenceError } from './errors'
 import { TimeParsingError } from './errors/TimeParsingError'
 
 const validationRegex =
-  /(\d{1,2})[:.,](\d{1,2})[.\s]*-[.\s]*(\d{1,2})[:.,](\d{1,2})/
+  /(\d{1,2})[:.,](\d{1,2})[.\s]*-?(?:[.\s]*(\d{1,2})[:.,](\d{1,2}))?/
 
 export interface ParsingResult {
   from: dayjs.Dayjs
@@ -26,10 +26,14 @@ export const useTimeParser = (
       .set('hour', parseInt(result[1]))
       .set('minute', parseInt(result[2]))
       .set('second', 0)
-    const to = dayjs()
-      .set('hour', parseInt(result[3]))
-      .set('minute', parseInt(result[4]))
-      .set('second', 0)
+
+    let to = dayjs(new Date())
+    if (result[3] && result[4]) {
+      to = to
+        .set('hour', parseInt(result[3]))
+        .set('minute', parseInt(result[4]))
+        .set('second', 0)
+    }
 
     return { from, to }
   })
