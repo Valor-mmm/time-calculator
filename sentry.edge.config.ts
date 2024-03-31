9febd1347753579d6c8de26bcacfcf4b821625f1
Valor-mmm <v_dev/github@slmail.me>
2024-03-31 23:57:41 +0200

    feat(monitoring): integrate sentry

---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+  dsn: 'https://13eb1abf9e2cc0362ecd63ca7dc7b61a@o4507004395651072.ingest.us.sentry.io/4507005091774464',
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
